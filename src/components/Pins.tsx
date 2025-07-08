e02f347f958dfd703278bb9d3b969e26337f4e4f
Zoey <z@zoey.ninja>
2025-07-08 14:14:15 -0400

    refactored some logic

--- src/components/Pins.tsx
+++ src/components/Pins.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { useAppContext } from '../context/AppContext'
 
 import { Marker } from 'react-map-gl/mapbox'
+import { Station } from 'radio-browser-api'
+import { simpleStationDiscription } from '@/utils/radioStations'
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
@@ -21,12 +23,45 @@ const Pin = ({ size = 20 }) => {
   )
 }
 
+interface StationModelProps {
+  station: Station
+  handleClose: () => void
+}
+
+const StationModel: React.FC<StationModelProps> = ({
+  station,
+  handleClose,
+}) => (
+  <dialog id="my_modal_5" className="modal modal-open">
+    <div className="modal-box">
+      <h3 className="font-bold text-lg">{station.name}</h3>
+      <p className="py-4">{simpleStationDiscription(station)}</p>
+      <div className="modal-action">
+        <form method="dialog">
+          <button className="btn" onClick={handleClose}>
+            Close
+          </button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+)
+
 export default function Pins() {
   const { radioStations } = useAppContext()
+  const [selectedStation, setSelectedStation] = React.useState<Station | null>(
+    null,
+  )
   // console.log('radioStations in Pins:', radioStations)
 
   return (
     <>
+      {!!selectedStation && (
+        <StationModel
+          station={selectedStation}
+          handleClose={() => setSelectedStation(null)}
+        />
+      )}
       {React.useMemo(
         () =>
           radioStations.map((radioStation) => {
@@ -43,7 +78,7 @@ export default function Pins() {
                     // If we let the click event propagates to the map, it will immediately close the popup
                     // with `closeOnClick: true`
                     e.originalEvent.stopPropagation()
-                    // setPopupInfo(radioStation)
+                    setSelectedStation(radioStation)
                   }}
                 >
                   <Pin />
--- src/utils/radioStations.ts
+++ src/utils/radioStations.ts
@@ -1,5 +1,13 @@
 import { Station } from 'radio-browser-api'
 
+export function combineStrings(strings: string[]): string {
+  return strings.join(', ')
+}
+
+export function simpleStationDiscription({ tags, language }: Station) {
+  return `${combineStrings(tags)} - ${combineStrings(language)}`
+}
+
 // as we add more stations to the state, make sure there are no duplicates
 export function removeDuplicatesById(array: Station[]): Station[] {
   const seenIds = new Set<string>() // Use Set for efficient lookups
