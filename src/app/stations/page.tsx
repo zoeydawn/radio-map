d6c9d281e880cd016d5f8cb41b7f86c6c3f9f932
Zoey <z@zoey.ninja>
2025-07-08 12:05:45 -0400

    added loader to list page

--- src/app/stations/page.tsx
+++ src/app/stations/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import Loader from '@/components/Loader'
 import StationList from '@/components/StationList'
 import { useAppContext } from '@/context/AppContext'
 
 export default function Stations() {
-  const { radioStations } = useAppContext()
+  const { radioStations, isLoading } = useAppContext()
   console.log('radioStations in list:', radioStations)
 
-  return <StationList stations={radioStations} header="this is a list" />
+  return (
+    <>
+      <StationList stations={radioStations} header="this is a list" />
+      {isLoading && <Loader />}
+    </>
+  )
 }
