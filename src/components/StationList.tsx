fa6dc5dd96cc3a277bc5a411e2bca103ae2b0022
Zoey <z@zoey.ninja>
2025-07-10 12:43:48 -0400

    added play button to station list

--- src/components/StationList.tsx
+++ src/components/StationList.tsx
@@ -2,6 +2,7 @@ import { simpleStationDiscription } from '@/utils/radioStations'
 import Image from 'next/image'
 import { Station } from 'radio-browser-api'
 import * as React from 'react'
+import PlayButton from './PlayButton'
 
 interface UserInfoProps {
   stations: Station[]
@@ -40,23 +41,8 @@ const StationList: React.FC<UserInfoProps> = ({ stations, header }) => {
                 {simpleStationDiscription(station)}
               </div>
             </div>
-            <button className="btn btn-square btn-ghost">
-              <svg
-                className="size-[1.2em]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M6 3L20 12 6 21 6 3z"></path>
-                </g>
-              </svg>
-            </button>
+
+            {/* heart button */}
             <button className="btn btn-square btn-ghost">
               <svg
                 className="size-[1.2em]"
@@ -74,6 +60,8 @@ const StationList: React.FC<UserInfoProps> = ({ stations, header }) => {
                 </g>
               </svg>
             </button>
+
+            <PlayButton station={station} />
           </li>
         )
       })}
