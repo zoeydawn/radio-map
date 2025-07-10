b49002e3df5e956cec5801e9cb3e3277579d41df
Zoey <z@zoey.ninja>
2025-07-10 15:38:14 -0400

    moved link component to new file

--- src/components/StationModel.tsx
+++ src/components/StationModel.tsx
@@ -4,46 +4,7 @@ import { locationString, simpleStationDiscription } from '@/utils/radioStations'
 import { useAppContext } from '../context/AppContext'
 import PlayButton from './PlayButton'
 import LikeButton from './LikeButton'
-import { Theme } from '@/context/AppContext.types'
-
-interface ExternalLinkProps {
-  href: string
-  rel: string
-  label: string
-  theme: Theme
-}
-
-const ExternalLink: React.FC<ExternalLinkProps> = ({
-  href,
-  rel,
-  label,
-  theme,
-}) => {
-  return (
-    <a
-      className={`${'link link-hover flex items-center pt-1 pb-1'} ${theme === 'dark' ? 'link-accent' : 'link-neutral'}`}
-      target="_blank"
-      href={href}
-      rel={rel}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-        />
-      </svg>{' '}
-      {label}
-    </a>
-  )
-}
+import ExternalLink from './ExternalLink'
 
 const StationModel: React.FC = ({}) => {
   const { viewedStation, setViewedStation, theme } = useAppContext()
@@ -55,7 +16,6 @@ const StationModel: React.FC = ({}) => {
   if (!viewedStation) {
     return null
   }
-  //   console.log('viewedStation:', viewedStation)
 
   return (
     <dialog id="my_modal_5" className="modal modal-open">
