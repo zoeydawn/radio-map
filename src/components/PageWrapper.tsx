b54f2404adee5df33601989e4c93b40c4a765717
Zoey <z@zoey.ninja>
2025-07-16 19:45:36 -0400

    more style improvements

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,12 +1,7 @@
 export default function PageWrapper({
-    children,
-  }: Readonly<{
-    children: React.ReactNode
-  }>) {
-    return (
-      <div className="pt-20">
-        {children}
-      </div>
-    )
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return <div className="pt-20 bg-base-200">{children}</div>
+}
