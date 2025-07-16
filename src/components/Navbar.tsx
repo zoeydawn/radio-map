368f0918e178f8116f31a1a529cf872c592f9e27
Zoey <z@zoey.ninja>
2025-07-16 13:46:42 -0400

    moved station list to home page component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,11 +30,11 @@ export default function Navbar() {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <Link href="/">Map</Link>
+              <Link href="/">Home</Link>
             </li>
-            <li>
+            {/* <li>
               <Link href="/stations">List</Link>
-            </li>
+            </li> */}
             <li>
               <Link href="/favorites">Favorites</Link>
             </li>
