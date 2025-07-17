a8616d3e4bf633742605320ba6488ea072a25b1f
Zoey <z@zoey.ninja>
2025-07-17 09:58:28 -0400

    fixed layout issue in the list view

--- src/components/StationList.tsx
+++ src/components/StationList.tsx
@@ -17,56 +17,58 @@ const StationList: React.FC<UserInfoProps> = ({
   stations,
   header,
   setViewedStation,
-  isLoading
+  isLoading,
 }) => {
   return (
-    <ul className="list bg-base-100 rounded-box shadow-md max-w-150 m-auto mt-4">
-      {!!header && (
-        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">{header}</li>
-      )}
-
-      {stations.map((station) => {
-        const { id, name, favicon } = station
-        const proxiedSrc = favicon
-          ? `/api/image-proxy?url=${encodeURIComponent(favicon)}`
-          : '/favicon.ico'
-
-        return (
-          <li className="list-row" key={`station-${id}`}>
-            <div
-              className="cursor-pointer"
-              onClick={() => setViewedStation(station)}
-            >
-              <Image
-                className="size-10 rounded-box"
-                alt={name}
-                src={proxiedSrc}
-                height={40}
-                width={40}
-                placeholder="blur"
-                blurDataURL={'/favicon.ico'}
-              />
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() => setViewedStation(station)}
-            >
-              <div>{name}</div>
-              <div className="text-xs uppercase font-semibold opacity-60">
-                {simpleStationDiscription(station)}
-              </div>
-            </div>
-
-            <LikeButton station={station} />
-
-            <PlayButton station={station} />
+    <div className="pb-50">
+      <ul className="list bg-base-100 rounded-box shadow-md max-w-150 m-auto mt-4">
+        {!!header && (
+          <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
+            {header}
           </li>
-        )
-      })}
-      <li>
-        {isLoading && <Loader />}
-      </li>
-    </ul>
+        )}
+
+        {stations.map((station) => {
+          const { id, name, favicon } = station
+          const proxiedSrc = favicon
+            ? `/api/image-proxy?url=${encodeURIComponent(favicon)}`
+            : '/favicon.ico'
+
+          return (
+            <li className="list-row" key={`station-${id}`}>
+              <div
+                className="cursor-pointer"
+                onClick={() => setViewedStation(station)}
+              >
+                <Image
+                  className="size-10 rounded-box"
+                  alt={name}
+                  src={proxiedSrc}
+                  height={40}
+                  width={40}
+                  placeholder="blur"
+                  blurDataURL={'/favicon.ico'}
+                />
+              </div>
+              <div
+                className="cursor-pointer"
+                onClick={() => setViewedStation(station)}
+              >
+                <div>{name}</div>
+                <div className="text-xs uppercase font-semibold opacity-60">
+                  {simpleStationDiscription(station)}
+                </div>
+              </div>
+
+              <LikeButton station={station} />
+
+              <PlayButton station={station} />
+            </li>
+          )
+        })}
+        <li>{isLoading && <Loader />}</li>
+      </ul>
+    </div>
   )
 }
 
