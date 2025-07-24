796eb914f05d69bf55a3321784acb9aa6b8b95d2
Zoey <z@zoey.ninja>
2025-07-24 15:53:22 -0400

    small layout update

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -134,7 +134,7 @@ const AudioPlayer: React.FC = () => {
 
   return (
     <div className="fixed bottom-0 left-0 w-full flex flex-col items-center shadow-sm">
-      <div className="rounded-lg bg-base-300 w-full max-w-150">
+      <div className="rounded-lg bg-base-300 w-full max-w-170">
         <div className="flex flex-col items-center space-y-4 p-3">
           <audio ref={audioRef} preload="metadata" className="hidden" />
           <h3
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { HeartIcon } from './Icons'
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 right-0 navbar bg-base-300 shadow-sm h-18 z-50">
-      <div className="flex items-center w-full max-w-150 m-auto">
+      <div className="flex items-center w-full max-w-200 m-auto">
         <div className="navbar-start">
           <NavDrawer />
         </div>
