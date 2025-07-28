0f114e3a4d2ca4e82a5c9da53972f523c2fc22be
Zoey <z@zoey.ninja>
2025-07-28 10:55:53 -0400

    updated play/pause logic to reset the station because there was a bug when playing paused station

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -21,105 +21,95 @@ const AudioPlayer: React.FC = () => {
 
   const urlResolved = selectedStation?.urlResolved
 
-  // Effect to handle autoplay and initial loading
-  useEffect(() => {
-    if (audioRef.current) {
-      const audio = audioRef.current
-      // Event listeners for updating state
-      const handleTimeUpdate = () => setCurrentTime(audio.currentTime)
-      const handleEnded = () => setIsPlaying(false)
-
-      audio.addEventListener('timeupdate', handleTimeUpdate)
-      audio.addEventListener('ended', handleEnded)
-
-      // Check if the URL is an HLS (.m3u8) stream
-      if (urlResolved?.endsWith('.m3u8')) {
-        // console.log('playing a .m3u8 stream!')
-        if (Hls.isSupported()) {
-          const hls = new Hls()
-          hlsInstanceRef.current = hls // Store the instance
-          hls.loadSource(urlResolved)
-          hls.attachMedia(audio)
-
-          hls.on(Hls.Events.ERROR, function (event, data) {
-            if (data.fatal) {
-              switch (data.type) {
-                case Hls.ErrorTypes.NETWORK_ERROR:
-                  console.error(
-                    'fatal network error encountered, try to recover',
-                    data,
-                  )
-                  hls.startLoad()
-                  break
-                case Hls.ErrorTypes.MEDIA_ERROR:
-                  console.error(
-                    'fatal media error encountered, try to recover',
-                    data,
-                  )
-                  hls.recoverMediaError()
-                  break
-                default:
-                  // cannot recover
-                  console.error('fatal error, destroying HLS instance', data)
-                  hls.destroy()
-                  hlsInstanceRef.current = null // Clear reference
-                  break
-              }
-              setPlayError('HLS error')
-            }
-          })
-        } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
-          // Fallback for browsers that natively support HLS (e.g., Safari)
-          audio.src = urlResolved
-        } else {
-          console.error(
-            'HLS is not supported in this browser, and native HLS playback is not available.',
-          )
-          setPlayError('HLS not supported in browser')
-        }
-      } else {
-        // For non-.m3u8 URLs, just set the src attribute directly
-        audio.src = urlResolved || ''
-      }
-
-      // Play when switching directly from one station to another
-      audioRef.current.play().catch((error) => {
-        console.error('Autoplay failed:', error)
-
-        setPlayError(error)
-        setIsPlaying(false)
-      })
-
-      // Cleanup event listeners on component unmount
-      return () => {
-        audio.removeEventListener('timeupdate', handleTimeUpdate)
-        audio.removeEventListener('ended', handleEnded)
-
-        // Cleanup HLS
-        if (hlsInstanceRef.current) {
-          hlsInstanceRef.current.destroy()
-          hlsInstanceRef.current = null
-        }
-        audio.src = '' // Ensure source is cleared
-      }
-    }
-  }, [selectedStation, urlResolved, setIsPlaying, setPlayError]) // Re-run if station prop changes
-
-  // so autoRef will respond to isPlaying
+  // Effect to handle initial loading and play/pause
   useEffect(() => {
     if (audioRef.current) {
       if (!isPlaying) {
         audioRef.current.pause()
       } else {
-        audioRef.current.play().catch((error) => {
-          console.error('Play failed:', error)
+        // it's better to reset all of this when playing a stream that has been paused,
+        // otherwise it tries to continue from where you left off which can break the stream if it's been paused for a long time
+        const audio = audioRef.current
+        // Event listeners for updating state
+        const handleTimeUpdate = () => setCurrentTime(audio.currentTime)
+        const handleEnded = () => setIsPlaying(false)
 
-          setPlayError(error) // Handle cases where play might be blocked (e.g., by browser policies)
+        audio.addEventListener('timeupdate', handleTimeUpdate)
+        audio.addEventListener('ended', handleEnded)
+
+        // Check if the URL is an HLS (.m3u8) stream
+        if (urlResolved?.endsWith('.m3u8')) {
+          // console.log('playing a .m3u8 stream!')
+          if (Hls.isSupported()) {
+            const hls = new Hls()
+            hlsInstanceRef.current = hls // Store the instance
+            hls.loadSource(urlResolved)
+            hls.attachMedia(audio)
+
+            hls.on(Hls.Events.ERROR, function (event, data) {
+              if (data.fatal) {
+                switch (data.type) {
+                  case Hls.ErrorTypes.NETWORK_ERROR:
+                    console.error(
+                      'fatal network error encountered, try to recover',
+                      data,
+                    )
+                    hls.startLoad()
+                    break
+                  case Hls.ErrorTypes.MEDIA_ERROR:
+                    console.error(
+                      'fatal media error encountered, try to recover',
+                      data,
+                    )
+                    hls.recoverMediaError()
+                    break
+                  default:
+                    // cannot recover
+                    console.error('fatal error, destroying HLS instance', data)
+                    hls.destroy()
+                    hlsInstanceRef.current = null // Clear reference
+                    break
+                }
+                setPlayError('HLS error')
+              }
+            })
+          } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
+            // Fallback for browsers that natively support HLS (e.g., Safari)
+            audio.src = urlResolved
+          } else {
+            console.error(
+              'HLS is not supported in this browser, and native HLS playback is not available.',
+            )
+            setPlayError('HLS not supported in browser')
+          }
+        } else {
+          // For non-.m3u8 URLs, just set the src attribute directly
+          audio.src = urlResolved || ''
+        }
+
+        // Play when switching directly from one station to another
+        audioRef.current.play().catch((error) => {
+          console.error('Autoplay failed:', error)
+
+          setPlayError(error)
           setIsPlaying(false)
         })
+
+        // Cleanup event listeners on component unmount
+        return () => {
+          audio.removeEventListener('timeupdate', handleTimeUpdate)
+          audio.removeEventListener('ended', handleEnded)
+
+          // Cleanup HLS
+          if (hlsInstanceRef.current) {
+            hlsInstanceRef.current.destroy()
+            hlsInstanceRef.current = null
+          }
+          audio.src = '' // Ensure source is cleared
+        }
       }
     }
-  }, [isPlaying, setPlayError, setIsPlaying])
+  }, [isPlaying, selectedStation, urlResolved, setIsPlaying, setPlayError]) // Re-run if station prop changes
 
   // Helper to format time
   const formatTime = (seconds: number) => {
